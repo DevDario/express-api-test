b428a92e5660083e185620481431283539c0aded
Dário Silva <dariosilva13222@gmail.com>
2023-12-29 23:36:41 +0100

    raw: creates a mysql pool

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const express = require('express')
 const app = express()
 
 const mysql = require('mysql')
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database:'products'
+})
