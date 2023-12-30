fe9e848d8cf236658dfafdd770ac8f49c075c0af
Dário Silva <dariosilva13222@gmail.com>
2023-12-30 14:16:57 +0100

    feat: handles form submit and other DOM events

---
+++ actions.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('new-user-form')
+
+form.addEventListener('submit', async (e) => {
+
+    e.preventDefault()
+
+    const name = String(document.getElementById('name-input').value)
+
+    if (name === "") return alert("You need to add a name")
+
+    try {
+        const response = await fetch('/api/newUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name })
+        })
+
+        const data = await response.json()
+        console.log(data.message);
+
+        alert('User Created Successfully')
+
+    } catch (error) {
+        console.log(error);
+    }
+
+})
